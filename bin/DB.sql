50514dcb01ae16543ad48a4917fb5c31664d1eb6
vinayN01 <vinaynelavai01@gmail.com>
2023-09-23 21:15:55 +0530

    Add first project

---
+++ bin/DB.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE ticket_system;
+
+USE ticket_system;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tickets (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    resolution TEXT,
+    product VARCHAR(20) NOT NULL
+); 
